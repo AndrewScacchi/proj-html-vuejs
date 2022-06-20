57ae44359518675df9306d2c448dc192ae1917dd
andrea scacchi <andrea.scacchi@live.com>
2022-06-20 10:15:32 +0200

    Initial Commit, added main scaffolding and img reference

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MyHeader
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  border: 1px solid red;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  #header {
+    width:100%;
+    height: 100px;
+    background: black;
+  }
+
+</style>
